672e5fc31fe1e2890aa4cac7262a8fa4a88be328
Anders <anoy@hotmail.no>
2026-04-12 23:28:42 +0200

    Add components

---
+++ src/components/FilterSidebar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>FilterSidebar</div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import FilterBar from '@/components/FilterBar.vue'
+import FilterSidebar from '@/components/FilterSidebar.vue'
+import RestaurantList from '@/components/RestaurantList.vue'
 </script>
 
 <template>
-  <main>
+  <main class="flex">
+    <FilterSidebar />
+    <div>
+      <FilterBar/>
+      <RestaurantList />
+    </div>
+
   </main>
 </template>
