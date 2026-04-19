4e24547ffa47e51daa2af3e7db488ac3dd37ed37
Anders <anoy@hotmail.no>
2026-04-20 00:34:34 +0200

    Style and add methods to RestaurantCard

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,39 +1,55 @@
 <script setup lang="ts">
 import type { Restaurant } from '@/types/restaurant-types'
+import { deliveryTimes } from '@/constants/filters.ts'
 
 const { restaurant } = defineProps<{
   restaurant: Restaurant
 }>()
 
+function formatDeliveryTime(deliveryTimeInMinutes: number) {
+  const timeRange = deliveryTimes.find(
+    (dt) => deliveryTimeInMinutes >= dt.min && deliveryTimeInMinutes < dt.max,
+  )
 
-
+  return timeRange?.label
+}
 </script>
 
 <template>
-  <div
-    class="card-shadow w-[327px] h-[202px] rounded-[8px] border-[0.6px] border-black/10 bg-white p-4 flex flex-col justify-between"
-  >
-    <div class="flex flex-row gap-1">
-      <div
-        class="flex flex-row gap-1 items-center card-shadow rounded-[88px] border-[0.6px] border-black/10 bg-white py-2 pr-3 pl-[10px]"
-      >
+<!--  Add hover effect-->
+  <button class="cursor-pointer">
+    <div
+      class="card-shadow w-[327px] h-[202px] rounded-[8px] border-[0.6px] border-black/10 bg-white p-4 flex flex-col justify-between"
+    >
+      <div class="flex flex-row gap-1">
+        <!--      Open status-->
+        <div
+          class="flex gap-1 items-center card-shadow rounded-[88px] border-[0.6px] border-black/10 bg-white py-2 pr-3 pl-[10px]"
+        >
+          <span
+            :class="restaurant.isCurrentlyOpen ? 'bg-[#00703A]' : 'bg-black'"
+            class="w-2 h-2 rounded-full"
+          />
+          <span class="text-[12px] font-normal leading-none tracking-[-0.5px]">{{
+            restaurant.isCurrentlyOpen ? 'Open' : 'Closed'
+          }}</span>
+        </div>
+        <!--      delivery time-->
         <span
-          :class="restaurant.isCurrentlyOpen ? 'bg-[#00703A]' : 'bg-black'"
-          class="w-2 h-2 rounded-full"
-        />
-        <span class="text-[12px] font-normal leading-none tracking-[-0.5px]">{{
-          restaurant.isCurrentlyOpen ? 'Open' : 'Closed'
-        }}</span>
+          class="card-shadow rounded-[88px] border-[0.6px] border-black/10 bg-white py-2 px-3 text-[12px] font-normal leading-none tracking-[-0.5px]"
+        >
+          {{ formatDeliveryTime(restaurant.deliveryTimeMinutes) }}
+        </span>
       </div>
-      <!--      delivery time-->
-      <div>{{ restaurant.deliveryTimeMinutes }}</div>
+      <div class="flex flex-row justify-between">
+        <h3 class="text-[24px] font-normal leading-none tracking-[-0.5px]">
+          {{ restaurant.name }}
+        </h3>
+        <!--      green button-->
+        <div></div>
+      </div>
     </div>
-    <div class="flex flex-row justify-between">
-      <h3 class="text-[24px] font-normal leading-none tracking-[-0.5px]">{{ restaurant.name }}</h3>
-      <!--      green button-->
-      <div></div>
-    </div>
-  </div>
+  </button>
 </template>
 
 <style scoped></style>
